a9ba742d3bb56a66a8b9f1cb2bd27c94dd9d345a
trynym03 <trny003@student.kristiania.no>
2025-01-21 15:29:17 +0100

    implimented map

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Application } from "./modules/app/application";
 
-createRoot(document.getElementById("root")!).render(<h1>Hello React</h1>);
+createRoot(document.getElementById("root")!).render(<Application />);
